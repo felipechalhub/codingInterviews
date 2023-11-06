888e87b53de819f36ca8f03d987bcdccc1965ca1
LAPTOP-62MA70CI\Felipe Portela <fp.chalhub@gmail.com>
2023-11-05 22:23:49 -0300

    Adding ValidParenthesis class

---
+++ codingInterviews/src/leetCode/ValidParenthesis.java
@@ -0,0 +1,15 @@
+package leetCode;
+
+import java.util.Iterator;
+
+public class ValidParenthesis {
+
+	public static void main(String[] args) {
+		
+		String s = "()";
+		for (int i = 0; i < s.length(); i++) {
+			
+		}
+	}
+
+}
